f4207b14ef87fd4d623efba8bdd0feb3267e262a
Mateusz Januszewski <mateuszj@Mateuszs-MacBook-Pro.local>
2020-09-02 14:43:29 +0100

    used echo

--- compose/web/start-server.sh
+++ compose/web/start-server.sh
@@ -3,11 +3,10 @@
 if [ -z ${PORT+x} ];
 then
     # PORT unset, we presume this is local dev:
-    print("If part 1")
+    echo  "Path 1"
     python manage.py migrate
     yarn serve
 else
     # PORT set, we presume this is Heroku:
-    print("If part 2")
     yarn django:serve:prod
 fi
